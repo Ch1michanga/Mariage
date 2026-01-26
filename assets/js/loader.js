0bedafce65377e6e9b8d8323a0c1e78d27f8cc89
Ch1michanga <83396020+Ch1michanga@users.noreply.github.com>
2026-01-26 18:50:12 +0100

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,6 +1,6 @@
 // assets/js/loader.js
 
-const VERSION = "3.11"; // incrémente uniquement ce chiffre
+const VERSION = "3.12"; // incrémente uniquement ce chiffre
 
 // Expose la version si besoin
 window.ASSET_VERSION = VERSION;
