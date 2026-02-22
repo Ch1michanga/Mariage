cf5468578c5960fec754220ea3df5d482a6628e3
Ch1michanga <83396020+Ch1michanga@users.noreply.github.com>
2026-02-22 17:09:33 +0100

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,6 +1,6 @@
 // assets/js/loader.js
 
-const VERSION = "3.56"; // incrémente uniquement ce chiffre
+const VERSION = "3.57"; // incrémente uniquement ce chiffre
 window.ASSET_VERSION = VERSION;
 
 // CSS
